f25c6580cd2a0e39c178bfd7da3c48723d8d038f
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2024-11-27 20:11:13 +0200

    Save commit

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-include 'Api\Transaction\transaction.php';
-include 'Api\Balance\balance.php';
+include 'Api/Transaction/transaction.php';
+include 'Api/Balance/balance.php';
